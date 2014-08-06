62a5fbed38caabd41b3665b301fe3eabc5ed7874
DSRoden <Daniel.S.Roden@gmail.com>
2014-08-06 07:15:51 -0500

    Added Task.collection, constructor, #insert, cPriority, .all, .sortByDate, .filterTags

--- app/models/task.js
+++ app/models/task.js
@@ -1,10 +1,67 @@
 'use strict';
 
+
+var Mongo = require('mongodb');
+var _ = require('lodash');
+
 function Task(o){
   this.name = o.name;
   this.due = new Date(o.due);
   this.photo = o.photo;
-  this.priorityId = Mongo.ObjectID(o.priorityId)
+  this.tags = [];
+  this.priorityId = Mongo.ObjectID(o.priorityId);
 }
 
+
+Object.defineProperty(Task, 'collection', {
+  get: function(){return global.mongodb.collection('tasks');}
+});
+
+Task.prototype.insert = function(cb){
+  Task.collection.save(this, cb);
+};
+
+
+Task.prototype.cPriority = function(newId, cb){
+  newId = Mongo.ObjectID(newId);
+  this.priorityId = newId;
+  
+  Task.collection.update({_id:this._id},{priorityId: newId}, cb);
+};
+
+Task.all = function(cb){
+  Task.collection.find().toArray(function(err, objects){
+    var tasks = objects.map(function(o){
+      return changePrototype(o);
+    });
+
+    cb(tasks);
+  });
+};
+
+Task.sortByDate = function(cb){
+  Task.collection.find().sort({'due': 1}).toArray(function(err, objects){
+    var tasks = objects.map(function(o){
+      return changePrototype(o);
+    });
+    cb(tasks);
+  });
+};
+
+Task.filterTags = function(query, cb) {
+  Task.collection.find({tags: {$in: [query]}}).toArray(function(err, objects){
+     var tasks = objects.map(function(o){
+      return changePrototype(o);
+     });
+     cb(tasks);
+  });
+};
+
 module.exports = Task;
+
+// PRIVATE HELPER FUNCTION
+
+function changePrototype(obj){
+  return _.create(Task.prototype, obj);
+}
+
--- test/unit/task.js
+++ test/unit/task.js
@@ -9,7 +9,80 @@ var Task = require('../../app/models/task');
 var dbConnect = require('../../app/lib/mongodb');
 var Mongo = require('mongodb');
 
+var high, low, jog, draw, write, compose, code, brainstorm, cycle, recycle;
+var apriority = [];
+
 describe('Task', function(){
+  before(function(done){
+    dbConnect('priority-test', function(){
+      done();
+    });
+  });
+
+  beforeEach(function(done){
+    Priority.collection.remove(function(){
+      var o = {name: 'High', color: 'red', value: '10'};
+      high = new Priority(o);
+      var o1 = {name: 'Low', color: 'yellow', value: '1'};
+      low = new Priority(o1);
+      
+      high.insert(function(){
+        low.insert(function(){
+          done();
+        });
+      });
+    });
+  });
+  
+  beforeEach(function(done){
+    Task.collection.remove(function(){
+    var o = {name:'jog', due: '01/06/2015', photo: 'http://www.nutripro.net/wp-content/uploads/2011/10/jogger.jpg'};
+    var o1 = {name:'draw', due: '01/02/2015', photo: 'http://www.nutripro.net/wp-content/uploads/2011/10/jogger.jpg'};
+    var o2 = {name:'write', due: '01/03/2015', photo: 'http://www.nutripro.net/wp-content/uploads/2011/10/jogger.jpg'};
+    var o3 = {name:'compose', due: '01/04/2015', photo: 'http://www.nutripro.net/wp-content/uploads/2011/10/jogger.jpg'};
+    var o4 = {name:'code', due: '01/05/2015', photo: 'http://www.nutripro.net/wp-content/uploads/2011/10/jogger.jpg'};
+    var o5 = {name:'brainstorm', due: '01/01/2015', photo: 'http://www.nutripro.net/wp-content/uploads/2011/10/jogger.jpg'};
+    var o6 = {name:'cycle', due: '01/07/2015', photo: 'http://www.nutripro.net/wp-content/uploads/2011/10/jogger.jpg'};
+    var o7 = {name:'recycle', due: '01/08/2015', photo: 'http://www.nutripro.net/wp-content/uploads/2011/10/jogger.jpg'};
+    jog = new Task(o);
+    draw = new Task(o1);
+    write = new Task(o2);
+    compose = new Task(o3);
+    code = new Task(o4);
+    brainstorm = new Task(o5);
+    cycle = new Task(o6);
+    recycle = new Task(o7);
+    
+    jog.tags.push('exercise', 'outdoors');
+    cycle.tags.push('exercise', 'outdoors');
+    draw.tags.push('creative', 'visual');
+    brainstorm.tags.push('visual', 'collaborative', 'creative');
+    compose.tags.push('musical', 'creative');
+    write.tags.push('literary', 'creative');
+    code.tags.push('creative', 'innovation');
+    recycle.tags.push('innovation');
+      
+    jog.insert(function(){
+        draw.insert(function(){
+          write.insert(function(){
+            compose.insert(function(){
+              code.insert(function(){
+                brainstorm.insert(function(){
+                  cycle.insert(function(){
+                    recycle.insert(function(){
+                      done();
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+ 
+ describe('constructor', function(){ 
   it('should create a task constructor', function(){
     var o = {name:'jog', due: '01/01/2015', photo: 'http://www.nutripro.net/wp-content/uploads/2011/10/jogger.jpg'};
     var jog = new Task(o);
@@ -17,9 +90,70 @@ describe('Task', function(){
     expect(jog).to.be.instanceof(Task);
     expect(jog.name).to.equal('jog');
     expect(jog.due).to.be.instanceof(Date);
-    expect(jog.photo).to.be.instanceof(String);
+    expect(jog.photo).to.be.a('string');
     expect(jog.tags).to.have.length(0);
   });
+ });
+
+
+ describe('#insert', function(){
+  it('should insert a task', function(done){
+    var o = {name:'jog', due: '01/01/2015', photo: 'http://www.nutripro.net/wp-content/uploads/2011/10/jogger.jpg'};
+    var jog = new Task(o);
+    jog.insert(function(){
+      expect(jog._id).to.be.instanceof(Mongo.ObjectID);  
+      done();
+    });
+  });
+ });
+ 
+ describe('#cPriority', function(){
+   it('should find a priority and add it to task', function(done){
+    apriority.push(high._id);
+    console.log('This is the priority object\'s id ',apriority[0]);
+      jog.cPriority(apriority[0], function(){
+        expect(jog.priorityId).to.equal(apriority[0]);
+        expect(jog.priorityId).to.be.instanceof(Mongo.ObjectID);
+        console.log('This is the priorityId for the task object ', jog.priorityId);
+        done();
+      });
+   });
+ });
+
+ describe('.all', function(){
+  it('should find all tasks', function(done) {
+    Task.all(function(tasks){
+      expect(tasks).to.have.length(8);
+      expect(tasks[0]).to.be.instanceof(Task);
+      done();
+    });
+  });
+ });
+
+ describe('.sortByDate', function(){
+  it('should sort tasks by due dates', function(done){
+    Task.sortByDate(function(tasks){
+      expect(tasks).to.have.length(8);
+      expect(tasks[0]).to.be.instanceof(Task);
+      expect(tasks[0].due).to.be.instanceof(Date);
+      expect(brainstorm.due).to.be.instanceof(Date);
+      //console.log(tasks);
+      done();
+    });
+  });
+ });
+
+ describe('.filterTags', function(){
+  it('should filter tasks by tag words', function(done){
+    Task.filterTags('creative', function(tasks){
+      expect(tasks).to.have.length(5);
+      console.log(tasks);
+      done();
+    });
+  });
+ });
+
+
 
 // Last Braces
 });
